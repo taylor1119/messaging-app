7108225e792886e6374bbb1624d2c245f5fb7680
taylor1119 <taylorcarter1119@gmail.com>
2022-10-24 23:44:21 +0100

    @client/Added constants folder

---
+++ packages/client/src/constants/envVars.ts
@@ -0,0 +1,5 @@
+export const API_ORIGIN =
+	process.env.REACT_APP_API_ORIGIN ?? 'http://localhost:4000/api';
+
+export const WS_ORIGIN =
+	process.env.REACT_APP_WS_ORIGIN ?? 'ws://localhost:4000/';
---
+++ packages/client/src/constants/reactQueryKeys.ts
@@ -0,0 +1,13 @@
+const queryKeys = {
+	friendsIds: ['user-ids', 'friends'],
+	friends: ['users', 'friends'],
+	receivedFriendRequests: ['friend-requests', 'received'],
+	sentFriendRequests: ['friend-requests', 'sent'],
+	activeFriends: ['users', 'friends', 'active'],
+	conversation: 'conversation',
+	friendRequestReceivers: ['users', 'friend-request-receiver'],
+	friendRequesters: ['users', 'friend-requesters'],
+	searchUsers: ['users', 'search'],
+};
+
+export default queryKeys;
