500ea432f2f0a1481a62be2599b9c2e01907e458
taylor1119 <taylorcarter1119@gmail.com>
2023-03-28 22:34:41 +0100

    Adjusted eslint configs

--- packages/eslint-config-custom-server/index.js
+++ packages/eslint-config-custom-server/index.js
@@ -1,17 +1,19 @@
 module.exports = {
+	env: {
+		es2021: true,
+		node: true,
+	},
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'prettier',
 	],
+	overrides: [],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module',
 	},
-	plugins: ['@typescript-eslint'],
-	env: {
-		node: true,
-		es6: true,
-	},
+	plugins: ['@typescript-eslint', 'turbo'],
+	rules: {},
 };
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,19 +1,21 @@
 module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
 	extends: [
 		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react-hooks/recommended',
 		'plugin:@typescript-eslint/recommended',
 		'prettier',
 	],
+	overrides: [],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
+		ecmaVersion: 'latest',
 		sourceType: 'module',
-		ecmaVersion: 2020,
 	},
-	plugins: ['@typescript-eslint'],
-	env: {
-		node: true,
-	},
-	rules: {
-		'@typescript-eslint/no-non-null-assertion': 'off',
-	},
+	plugins: ['react', '@typescript-eslint'],
+	rules: { 'react/react-in-jsx-scope': 'off' },
 };
