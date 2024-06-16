e0c4952256c0a6c93ff640a856bee9ac1481b1b0
taylor1119 <taylorcarter1119@gmail.com>
2024-06-17 00:02:07 +0100

    @web/fixed friends list styling issue

--- apps/web/src/components/FriendsList/index.tsx
+++ apps/web/src/components/FriendsList/index.tsx
@@ -78,7 +78,7 @@ const FriendsListItem = ({
 				</ConditionalWrapper>
 			</ListItemAvatar>
 			<ListItemText
-				sx={{ display: { xs: 'none', md: 'inherit' }, ml: '12px' }}
+				sx={{ display: { xs: 'none', md: 'inherit' }, px: '12px' }}
 				primary={friend.userName}
 			/>
 		</ListItem>
@@ -98,7 +98,7 @@ const FriendsList = () => {
 				'& .MuiDrawer-paper': {
 					width: { xs: '103px', md: '360px' },
 					boxSizing: 'border-box',
-					mt: { xs: '56px', sm: '64px' },
+					pt: { xs: '56px', sm: '64px' },
 				},
 
 				zIndex: 0,
@@ -108,9 +108,8 @@ const FriendsList = () => {
 		>
 			<List
 				sx={{
-					width: '360px',
 					mx: '8px',
-					mt: '8px',
+					pt: '8px',
 					'& .MuiButtonBase-root': {
 						borderRadius: '8px',
 						':hover': {
